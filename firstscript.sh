ff88cb2261ca8ccce8d2a11dc8a180558add22a1
Zion <zionholtsc@gmail.com>
2024-02-10 15:57:40 -0500

    Created directory and multiple scripts

---
+++ firstscript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+# What's above is known as the shebang.
+
+echo Hello World! # When the script is executed, the echo command prints Hello World! to standard output.
---
+++ variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+number_one=200
+
+anothervar=John
+
+echo $number_one $anothervar
+echo # good for getting a blank line on the screen
+
+sampledir=~/
+
+ls $sampledir
